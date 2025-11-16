5ffc5e80520d6820ff2bbfb771c236d7c1c5d89a
jddunn <johnnyfived@protonmail.com>
2025-11-16 08:33:20 -0800

    chore(sdk): disable commit body line length check

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     'subject-case': [1, 'never', ['start-case', 'pascal-case', 'upper-case']],
     'header-max-length': [2, 'always', 120],
+    'body-max-line-length': [0, 'always'],
   },
 };
 
